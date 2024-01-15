ff4d4caddd0b057850030c25beef3c96a17a2d7b
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-01-15 20:27:43 +0900

    Time: 46 ms (65.87%), Space: 15.8 MB (26.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,12 +1,20 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        for i in 0..<nums.count {
-            for j in i+1..<nums.count {
-                if nums[i] + nums[j] == target {
-                    return [i, j]
-                }
+        var dict = [Int: Int]()
+
+        for (index, num) in nums.enumerated() {
+            if let addent = dict[num] {
+                return [addent, index]
+            } else {
+                dict[target - num] = index
             }
-        }        
+            // var complement: Int = target - num
+            // if let comValue = dict.first(where: { $0.key == complement }) {
+            //     return [comValue.value, index]
+            // } else {
+            //     dict[num] = index
+            // }
+        }
         return []
     }
 }
