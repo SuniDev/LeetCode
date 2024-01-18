f6386fde1ae4d9cf1c12efe330a8e89126773963
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-01-18 23:26:22 +0900

    Time: 31 ms (59.32%), Space: 15.8 MB (6.60%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.swift
+++ 0009-palindrome-number/0009-palindrome-number.swift
@@ -1,16 +1,5 @@
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        if x >= 0 {
-            var result = 0
-            var copyX = x
-            while copyX != 0 {
-                result *= 10
-                result += copyX % 10
-                copyX /= 10
-            }
-            return result == x
-        } else {
-            return false
-        }
+        return String(x) == String(String(x).reversed())      
     }
 }
