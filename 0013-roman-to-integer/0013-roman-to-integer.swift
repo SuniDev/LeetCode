0219d6c97b13e00e4a2fd9f34acfb60dbc1a230e
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-01-22 20:54:45 +0900

    Time: 16 ms (52.76%), Space: 15.8 MB (19.17%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.swift
+++ 0013-roman-to-integer/0013-roman-to-integer.swift
@@ -1,13 +1,18 @@
 class Solution {
     func romanToInt(_ s: String) -> Int {
-        let dict: [Character:Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
-        var out = 0, prev = 0
-        for i in s {
-            let val = dict[i] ?? 0
-            out += (val <= prev) ? prev : -prev
-            prev = val
+        let roman: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
+        
+        var out = 0
+        var prev = 0
+        
+        for char in s {
+            let current = roman[char] ?? 0
+            out += (current <= prev) ? prev : -prev
+            prev = current
         }
+        
         out += prev
+
         return out
     }
 }
