3d1482be6ca29d14f0862451ec7613f9a3ee5320
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-02-07 21:50:38 +0900

    Time: 5 ms (55.08%), Space: 15.9 MB (25.92%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+        var ans = 0
+
+        for c in s.reversed() {
+            if c != " " {
+                ans += 1
+            } else if ans > 0 {
+                return ans
+            }
+        }
+
+        return ans
+    }
+}
